9eabd572c98064216dfc89614bf1d896c1aacdc8
ahsan-swr <ahsan@tapforce.com>
2025-03-24 13:20:17 +0500

    Change mobile nav layout

--- src/routes/(doc)/+layout.svelte
+++ src/routes/(doc)/+layout.svelte
@@ -3,23 +3,32 @@
 	import Logo from '$lib/components/logo.svelte';
 	import ThemeToggle from '$lib/components/theme-toggle.svelte';
 	import { MetaMask } from '$lib/metamask/metamask.svelte';
+	import { Menu } from '@lucide/svelte';
 	import MetamaskPanel from './metamask-panel.svelte';
 	import ProfileMenu from './profile-menu.svelte';
 	import { SidebarMenuStore } from './sidebar-menu-store.svelte';
 	import SidebarMenu from './sidebar-menu.svelte';
 	import SidebarMobile from './sidebar-mobile.svelte';
 	import VersionController from './version-controller.svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	const mm = MetaMask.create();
 
 	let { children } = $props();
 
 	let pageContentEl = $state<HTMLDivElement>();
+	let isMobileSidebarOpen = $state(false);
 
 	const sidebar = SidebarMenuStore.create();
 
 	$effect(() => {
 		return sidebar.tocTracking(pageContentEl, page.url);
+	});
+
+	$effect(() => {
+		page.url.pathname; // Needed to track this as dependency
+
+		isMobileSidebarOpen = false;
 	});
 </script>
 
@@ -42,7 +51,12 @@
 			class="sticky top-0 z-[2] flex h-14 flex-none items-center justify-start gap-2 border-b bg-background px-3"
 		>
 			<div class="lg:hidden">
-				<SidebarMobile />
+				<Button
+					size="icon"
+					variant="outline"
+					onclick={() => (isMobileSidebarOpen = !isMobileSidebarOpen)}><Menu /></Button
+				>
+				<!-- <SidebarMobile /> -->
 			</div>
 
 			<VersionController />
@@ -53,6 +67,12 @@
 				<ProfileMenu />
 			</div>
 		</div>
+
+		{#if isMobileSidebarOpen}
+			<div class="fixed bottom-0 left-0 right-0 top-14 z-[999] bg-background">
+				<SidebarMenu />
+			</div>
+		{/if}
 
 		<div class="flex flex-1 flex-col" bind:this={pageContentEl}>
 			{@render children()}
