695f51917bd6bc3d20346ddc9013fac1da5c7585
TrungRueta <trungrueta@icloud.com>
2025-03-27 01:06:33 +0700

    update

--- src/routes/(doc)/+page.server.ts
+++ src/routes/(doc)/+page.server.ts
@@ -2,5 +2,9 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
+	// const version = Subdomain.test(url);
+	// if (version) {
+	// 	return { version };
+	// }
 	throw redirect(302, '/v1');
 }) satisfies PageServerLoad;
