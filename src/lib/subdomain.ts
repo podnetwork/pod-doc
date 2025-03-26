1768e57a01e34f4a123363331e52c98f1189f8bc
TrungRueta <trungrueta@icloud.com>
2025-03-27 01:06:06 +0700

    fix: clarify subdomain vercel

--- src/lib/subdomain.ts
+++ src/lib/subdomain.ts
@@ -1,9 +1,9 @@
 
 export class Subdomain {
-    static readonly DomainRegex = /pod-doc-svelte-(v\d+)\.vercel\.app/;
+    static readonly DomainVercelRegex = /^https?:\/\/pod-doc-svelte-(v\d+)\.vercel\.app/;
 
     static test(url: URL): string | null {
-        const match = url.origin.match(this.DomainRegex);
+        const match = url.origin.match(this.DomainVercelRegex);
         return match ? match[1] : null;
     }
 }
