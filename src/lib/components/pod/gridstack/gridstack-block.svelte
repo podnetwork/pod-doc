80b6631fff796edea9097ab9bdee926b6c7a20e5
TrungRueta <trungrueta@icloud.com>
2025-05-02 21:01:05 +0700

    add: grid block support link

--- src/lib/components/pod/gridstack/gridstack-block.svelte
+++ src/lib/components/pod/gridstack/gridstack-block.svelte
@@ -1,29 +1,44 @@
 <script lang="ts">
-	let { children } = $props();
+	import type { Snippet } from 'svelte';
+
+	let {
+		children,
+		href
+	}: {
+		children: Snippet;
+		href?: string;
+	} = $props();
+
+	let dynamicTag = $derived.by(() => (href ? 'a' : 'div'));
 </script>
 
 <div class="gridstack-block">
-	<div class="">
-        {@render children()}
+	<svelte:element this={dynamicTag as 'a' | 'div'} {href} class="h-full block">
+		{@render children()}
 		<div class="top">&nbsp;</div>
 		<div class="bottom">&nbsp;</div>
-	</div>
+	</svelte:element>
 </div>
 
 <style lang="postcss">
 	.gridstack-block {
 		@apply overflow-hidden;
 
-		:global(> div) {
+		:global(> div, > a) {
 			box-sizing: border-box;
 			position: relative;
 			@apply border bg-muted px-8 py-8;
 		}
 
+		:global(> a) {
+			@apply cursor-pointer no-underline text-inherit;
+			@apply hover:opacity-70 transition-opacity duration-200;
+		}
+
 		.top,
 		.bottom {
 			@apply absolute left-0 top-0 size-full;
-            @apply pointer-events-none;
+			@apply pointer-events-none;
 		}
 
 		.top:before,
@@ -32,7 +47,7 @@
 		.bottom:after {
 			content: ' ';
 			@apply absolute;
-			@apply border border-border dark:border-background bg-background;
+			@apply border border-border bg-background dark:border-background;
 			width: 40px;
 			height: 40px;
 		}
@@ -59,11 +74,10 @@
 			//box-shadow: inset 1px 1px 1px grey;
 		}
 
-
-        :global(h1, h2, h3, h4, h5, h6) {
-            &:first-child {
-                @apply mt-0 mb-2;
-            }
-        }
+		:global(h1, h2, h3, h4, h5, h6) {
+			&:first-child {
+				@apply mb-2 mt-0;
+			}
+		}
 	}
 </style>
