a08ce682f9018a8c7023034dc9036495e83cbd2e
TrungRueta <trungrueta@icloud.com>
2025-05-05 01:26:47 +0700

    feat: layout no longer require pass runtime TOC

--- src/lib/mdlayouts/single.svelte
+++ src/lib/mdlayouts/single.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
 	import { App } from '$lib/app.svelte';
-	import { untrack } from 'svelte';
-	import { SidebarMenuStore } from '../../routes/(doc)/sidebar-menu-store.svelte';
 
-	let { children, toc, url, code_languages } = $props();
+	let { children, code_languages } = $props();
 
 	const app = App.get();
-	const s = SidebarMenuStore.get();
-
-	$effect(() => {
-		if (toc && url) {
-			s.headings(url, toc);
-			untrack(() => {
-				s.makeItems()
-			})
-		}
-	});
 
 	$effect(() => {
 		if (code_languages) {
