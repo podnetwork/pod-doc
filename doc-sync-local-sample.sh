082df4938d25f44c6904f64fcd339d500ab05cfb
TrungRueta <trungrueta@icloud.com>
2025-05-05 01:17:12 +0700

    feat: update instruction

--- doc-sync-local-sample.sh
+++ doc-sync-local-sample.sh
@@ -3,12 +3,10 @@
 # this is template script help develop pod document content locally.
 # 1. copy this file to new file with name doc-sync-local.sh
 # 2. update the command below following:
-#   - <version> = version of document on pod doc site
-#   - <content branch name> = branch name on markdown content repo pulled to local
 #   - <local folder> = local folder path to folder contains markdown content on content repo
 # 
 # 3. run script by npm (or pnpm) to sync files from content repo to pod doc repo:
 #   - npm run docsync:local
 #   - pnpm run docsync:local
 
-sh ./doc-sync.sh "<version>:<content branch name>" "<local folder>"
+sh ./doc-sync.sh "local" "<local folder>"
